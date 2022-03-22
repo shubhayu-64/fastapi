df078b8b7baa8bec5e9a52e4fce73d4be1b5153c
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-03-22 09:16:33 +0530

    hehe

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, APIRouter
-
-
-
+import posts
 
 app = FastAPI()
+
+app.include_router(posts.router)
 
 @app.get("/")
 async def root():
--- app/posts/posts.py
+++ app/posts/posts.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 from ..models import Post
 
+router = APIRouter()
 
-
-@app.get("/posts")
+@router.get("/posts")
 async def get_posts():
     return {"data": "Post data"}
 
 
-@app.post("/posts")
+@router.post("/posts")
 async def create_posts(post: Post):
     return {"data": post.dict()}
